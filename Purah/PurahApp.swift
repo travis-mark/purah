7b3b5e71e5b5eac0eee5d7dda9c48709a1baf066
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-05-30 22:32:04 -0400

    Initial Commit

---
+++ Purah/PurahApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurahApp.swift
+//  Purah
+//
+//  Created by Travis Luckenbaugh on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct PurahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
