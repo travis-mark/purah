685f2e659af327c403fe1dd80f24ab7b6941be5a
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-06-05 23:08:30 -0400

    Refactor to allow more measurements, add body fat

--- Purah/VitalsView.swift
+++ Purah/VitalsView.swift
@@ -4,47 +4,67 @@
 import SwiftUI
 import HealthKit
 
-// TODO: Vitals View
+struct HKQuantityViewModel {
+    let quantityTypeLocalizedString: String
+    let quantityValue: Double
+    let quantityUnitLocalizedString: String
+    let uuid: UUID
+}
+
+extension HKQuantitySample {
+    var quantityViewModel: HKQuantityViewModel {
+        switch quantityType {
+        case HKQuantityType.quantityType(forIdentifier: .bodyMass):
+            return HKQuantityViewModel(quantityTypeLocalizedString: NSLocalizedString("Weight", comment: "Body mass"), quantityValue: quantity.doubleValue(for: .pound()), quantityUnitLocalizedString: NSLocalizedString("lbs", comment: "pounds"), uuid: uuid)
+        case HKQuantityType.quantityType(forIdentifier: .bodyFatPercentage):
+            return HKQuantityViewModel(quantityTypeLocalizedString: NSLocalizedString("Body Fat", comment: "Body fat"), quantityValue: quantity.doubleValue(for: .count()) * 100, quantityUnitLocalizedString: NSLocalizedString("%", comment: "percent"), uuid: uuid)
+        default:
+            return HKQuantityViewModel(quantityTypeLocalizedString: quantityType.identifier, quantityValue: quantity.doubleValue(for: .count()), quantityUnitLocalizedString: NSLocalizedString("each", comment: "each"), uuid: uuid)
+        }
+    }
+}
+
 struct VitalsView: View {
     private let healthStore = HKHealthStore()
     @State private var samples: [HKQuantitySample] = []
     
     var body: some View {
-        List(samples, id: \.uuid) { sample in
-            Text("Weight")
-            Text("\(sample.quantity.doubleValue(for: .pound()))")
+        List(samples.map({ $0.quantityViewModel }), id: \.uuid) { vm in
+            HStack {
+                Text(vm.quantityTypeLocalizedString)
+                Spacer()
+                Text("\(vm.quantityValue) \(vm.quantityUnitLocalizedString)")
+            }
         }.onAppear {
             requestAuthorization()
         }
     }
     
     private func requestAuthorization() {
-        let typesToRead: Set<HKObjectType> = [HKObjectType.quantityType(forIdentifier: .bodyMass)!]
+        // TODO: More sample types
+        let typesToRead: Set<HKSampleType> = [HKSampleType.quantityType(forIdentifier: .bodyMass)!, HKSampleType.quantityType(forIdentifier: .bodyFatPercentage)!]
         
         healthStore.requestAuthorization(toShare: nil, read: typesToRead) { success, error in
             if success {
-                fetchWeightMeasurements()
+                for sampleType in typesToRead {
+                    fetchMeasurements(for: sampleType)
+                }
             } else if let error = error {
                 print("Error requesting authorization: \(error.localizedDescription)")
             }
         }
     }
     
-    private func fetchWeightMeasurements() {
-        guard let weightType = HKSampleType.quantityType(forIdentifier: .bodyMass) else {
-            print("Weight type is not available.")
-            return
-        }
-        
-        let query = HKSampleQuery(sampleType: weightType, predicate: nil, limit: 1, sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)]) { query, results, error in
+    private func fetchMeasurements(for sampleType: HKSampleType) {
+        let query = HKSampleQuery(sampleType: sampleType, predicate: nil, limit: 1, sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)]) { query, results, error in
             if let error = error {
-                print("Error querying weight measurements: \(error.localizedDescription)")
+                print("Error querying measurements: \(error.localizedDescription)") // TODO: console.error
                 return
             }
             
             if let samples = results as? [HKQuantitySample] {
                 DispatchQueue.main.async {
-                    self.samples = samples
+                    self.samples = self.samples.filter({ $0.sampleType != sampleType }) + samples
                 }
             }
         }
